f2798cd27a8821eac827e424bc8c2bfe5c65e725
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-16 16:01:10 +0900

    [FEAT] local profile 용 kafka & rabbitmq 세팅

---
+++ start-local.sh
@@ -0,0 +1,2 @@
+docker-compose -f ./rabbitmq/local/docker-compose.yml up -d
+docker-compose -f ./kafka/local/docker-compose.yml up -d
---
+++ stop-local.sh
@@ -0,0 +1,2 @@
+docker-compose -f ./rabbitmq/local/docker-compose.yml down
+docker-compose -f ./kafka/local/docker-compose.yml down
