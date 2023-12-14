6561b62c1f5635c794b60fea50a46e473a8d952f
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-14 10:24:44 +0900

    [FIX] 파일 이름 수정 및 dev 환경 로직 수정

--- start-dev.sh
+++ start-dev.sh
@@ -4,50 +4,27 @@ kubectl label node dailyon-m02 type=App
 kubectl label node dailyon-m02 size=Medium
 kubectl label node dailyon-m03 type=Util
 kubectl label node dailyon-m03 size=Small
+kubectl label node dailyon-m04 type=Kafka
+kubectl label node dailyon-m05 type=Redis
 
 kubectl create -f ./kube-config/dev/namespace.yml
 kubectl create -f ./kube-config/dev/mysql-config.yml
 
-kubectl create -f ./kafka/dev/deployment-kafka.yml
-kubectl create -f ./kafka/dev/service-kafka.yml
-
-while ! kubectl get services kafka-service -n dev &> /dev/null; do
-  echo "Waiting for kafka-service to become available..."
-  sleep 5
-done
+kubectl create -f ./kafka/dev/deployment.yml
+kubectl create -f ./kafka/dev/service.yml
 
 kubectl create -f ./rabbitmq/dev/deployment.yml
 kubectl create -f ./rabbitmq/dev/service.yml
 
-while ! kubectl get services rabbitmq-service -n dev &> /dev/null; do
-  echo "Waiting for rabbitmq-service to become available..."
-  sleep 5
-done
-
 kubectl create -f ./discovery-service/deployment.yml
 kubectl create -f ./discovery-service/service.yml
-
-while ! kubectl get services discovery-service -n dev &> /dev/null; do
-  echo "Waiting for discovery-service to become available..."
-  sleep 5
-done
 
 kubectl create configmap dailyon-config --from-env-file=./config-service/.env -n dev
 kubectl create -f ./config-service/deployment.yml
 kubectl create -f ./config-service/service.yml
 
-while ! kubectl get services config-service -n dev &> /dev/null; do
-  echo "Waiting for config-service to become available..."
-  sleep 5
-done
-
 kubectl create -f ./apigateway-service/deployment.yml
 kubectl create -f ./apigateway-service/service.yml
-
-while ! kubectl get services apigateway-service -n dev &> /dev/null; do
-  echo "Waiting for apigateway-service to become available..."
-  sleep 5
-done
 
 kubectl create -f ./auth-service/initdb-config.yml
 kubectl create -f ./auth-service/deployment.yml
